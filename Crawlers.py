7e3216e7b21f24645c26f05a81ce19d62d3f5505
serah <serah4291@gmail.com>
2012-05-09 16:19:29 +0530

    making the code more generic

--- Crawlers.py
+++ Crawlers.py
@@ -5,168 +5,168 @@ import re
 from BeautifulSoup import BeautifulSoup
 
 
-class ninecircuits:
+class site1:
 	
 	def initialize(self):
 		# access the required page
-		page = urllib2.urlopen("http://9circuits.com/store/products/?items_per_page=all")
+		page = urllib2.urlopen("site url")
 		self.soup = BeautifulSoup(page)
-		self.product_price()
+		self.info1()
 		
-	def product_price(self): # get all the product prices and save in a list
-		productprices=[]
-		# search for all occurrences of 'pricedisplay' in the html tags
-		productprice = self.soup.findAll(attrs="pricedisplay")
-		for p in productprice:
-			# add the prices of products in the pricelist
-			productprices.append(p.string)
-		return productprices
-		self.product_name()
+	def info1(self): # get all relevant and save in a list
+		infoList1=[]
+		# search for all occurrences of 'required data' in the html tags
+		info1 = self.soup.findAll(attrs="attribute")
+		for p in info1:
+			# add the data in the list
+			infoList1.append(p.string)
+		return infoList1
+		self.info2()
 					
-	def product_name(self): # get all product names and save in 'productnames'
-		productnames=[]
+	def info2(self): # get all required info and save in second  list
+		infoList2=[]
 		productname = self.soup.findAll("strong")
 		for pName in productname:
-			#add the names of products in the product list
-			productnames.append(pName.string)
-		return productnames
+			#add the 'relevant data' list
+			infoList2.append(pName.string)
+		return infoList2
 
-class explorelabs:
+class site2:
 
 	def initialize(self):
-		page = urllib2.urlopen("http://store.explorelabs.com/index.php?main_page=products_all")
+		page = urllib2.urlopen("site url")
 		self.soup = BeautifulSoup(page)
-		self.product_names()
+		self.info2()
 		
-	def product_names(self):
-		productName = self.soup.findAll(colspan = '2')
-		productnames = []
-		for pName in productName:
-			productnames.append(pName.contents[1].next.next)
-		return productnames
-		self.product_prices()
+	def info2s(self):
+		getInfo = self.soup.findAll(colspan = '2')
+		infoList2 = []
+		for pName in getInfo:
+			infoList2.append(pName.contents[1].next.next)
+		return infoList2
+		self.info1s()
 		
-	def product_prices(self):
-		productPrice = self.soup.findAll(colspan = '2')		
-		productprices = []
-		for p in productPrice:
-			productprices.append(p.contents[5])
-		return productprices
+	def info1(self):
+		getInfo1 = self.soup.findAll(colspan = '2')		
+		infoList1 = []
+		for p in getInfo1:
+			infoList1.append(p.contents[5])
+		return infoList1
 
-class mgSuperStore: # get product names and their prices from all pages
+class site3: # get relevant information
 	
 	def get_data(self):
-		productprices = []
-		productnames = []
+		infoList1 = []
+		infoList2 = []
 		
-		for u in ("http://mgsuperlabs.co.in/store/index.php/cPath/1","http://mgsuperlabs.co.in/store/index.php/cPath/2","http://mgsuperlabs.co.in/store/index.php/cPath/2_7","http://mgsuperlabs.co.in/store/index.php/cPath/2_6","http://mgsuperlabs.co.in/store/index.php/cPath/2_8","http://mgsuperlabs.co.in/store/index.php/cPath/5","http://mgsuperlabs.co.in/store/index.php/cPath/9"):
+		for u in ("web page"):
 			page=urllib2.urlopen(u)
 			soup = BeautifulSoup(page)
 					
-			productName = soup.findAll("td",align = None)
-			for p in productName:
-				if p.next.next.string != 'Product Name+':
-					productnames.append(p.next.next.string)
+			getInfo = soup.findAll("td",align = None)
+			for p in getInfo:
+				if p.next.next.string != 'info to be skipped':
+					infoList2.append(p.next.next.string)
 						
-			productPrice = soup.findAll('td',align = 'right')		
-			for p in productPrice:
+			getInfo1 = soup.findAll('td',align = 'right')		
+			for p in getInfo1: #taking care of exceptions in tags
 				try:
-					productprices.append(p.contents[2].string) #product price in case of no sale
+					infoList1.append(p.contents[2].string) 
 				except:
-					if p.contents[0].string !='Price':
-						productprices.append(p.contents[0].string) # price of product on sale
+					if p.contents[0].string !='the data to be skipped':
+						infoList1.append(p.contents[0].string) 
 			page.close()
 		
-		return productnames,productprices	
+		return infoList2,infoList1	
 		
-class simplelabs:
+class site4:
 			
 	def get_data(self):
-		productprices = []
-		productnames = []
+		infoList1 = []
+		infoList2 = []
 		
-		for u in ('http://www.simplelabs.co.in/catalog/arduino-boards','http://www.simplelabs.co.in/catalog/arduino-shields','http://www.simplelabs.co.in/catalog/starter-kits','http://www.simplelabs.co.in/catalog/other-boards','http://www.simplelabs.co.in/catalog/pressure-force-flow','http://www.simplelabs.co.in/catalog/distance-measurement','http://www.simplelabs.co.in/catalog/accelerometers-gyros','http://www.simplelabs.co.in/catalog/temperature-light','http://www.simplelabs.co.in/catalog/other-sensors','http://www.simplelabs.co.in/catalog/robotics-kits','http://www.simplelabs.co.in/catalog/ics','http://www.simplelabs.co.in/catalog/motors','http://www.simplelabs.co.in/catalog/wireless','http://www.simplelabs.co.in/catalog/discrete-components','http://www.simplelabs.co.in/catalog/displays'):
+		for u in ('relevant web pages'):
 			page=urllib2.urlopen(u)
 			soup = BeautifulSoup(page)
 					
-			productName = soup.findAll("td", attrs = {'class'  : 'catalogueItem'})
-			for p in productName:
-				productnames.append(p.h2.next.next)
-			productPrice = soup.findAll(attrs = 'uc-price')		
-			for p in productPrice:
-				productprices.append(p.contents[0])
+			getInfo = soup.findAll("td", attrs = {'class'  : 'class'})
+			for p in getInfo:
+				infoList2.append(p.h2.next.next)
+			getInfo1 = soup.findAll(attrs = 'attribute')		
+			for p in getInfo1:
+				infoList1.append(p.contents[0])
 			page.close()						
 		
-		return productnames,productprices
+		return infoList2,infoList1
 				
-class rhydolabz:
+class site5:
 			
 	def get_data(self):
-		productprices = []
-		productnames = []
+		infoList1 = []
+		infoList2 = []
 		
-		for u in ("http://www.rhydolabz.com/index.php?main_page=index&cPath=152_123","http://www.rhydolabz.com/index.php?main_page=index&cPath=152_151","http://www.rhydolabz.com/index.php?main_page=index&cPath=152_169","http://www.rhydolabz.com/index.php?main_page=index&cPath=88","http://www.rhydolabz.com/index.php?main_page=index&cPath=67","http://www.rhydolabz.com/index.php?main_page=index&cPath=99","http://www.rhydolabz.com/index.php?main_page=index&cPath=155_161","http://www.rhydolabz.com/index.php?main_page=index&cPath=155_156","http://www.rhydolabz.com/index.php?main_page=index&cPath=155_157",'http://www.rhydolabz.com/index.php?main_page=index&cPath=155_160','http://www.rhydolabz.com/index.php?main_page=index&cPath=155_162',"http://www.rhydolabz.com/index.php?main_page=index&cPath=155_163","http://www.rhydolabz.com/index.php?main_page=index&cPath=155_164","http://www.rhydolabz.com/index.php?main_page=index&cPath=155_167","http://www.rhydolabz.com/index.php?main_page=index&cPath=87","http://www.rhydolabz.com/index.php?main_page=index&cPath=80","http://www.rhydolabz.com/index.php?main_page=index&cPath=130_131","http://www.rhydolabz.com/index.php?main_page=index&cPath=130_132","http://www.rhydolabz.com/index.php?main_page=index&cPath=130_133","http://www.rhydolabz.com/index.php?main_page=index&cPath=130_134","http://www.rhydolabz.com/index.php?main_page=index&cPath=130_136","http://www.rhydolabz.com/index.php?main_page=index&cPath=130_135","http://www.rhydolabz.com/index.php?main_page=index&cPath=98","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_138","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_139","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_144","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_145","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_140","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_141","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_142","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_143","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_150","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_146","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_149","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_147","http://www.rhydolabz.com/index.php?main_page=index&cPath=137_148","http://www.rhydolabz.com/index.php?main_page=index&cPath=97_111","http://www.rhydolabz.com/index.php?main_page=index&cPath=97_112","http://www.rhydolabz.com/index.php?main_page=index&cPath=72","http://www.rhydolabz.com/index.php?main_page=index&cPath=82","http://www.rhydolabz.com/index.php?main_page=index&cPath=124","http://www.rhydolabz.com/index.php?main_page=index&cPath=126","http://www.rhydolabz.com/index.php?main_page=index&cPath=166","http://www.rhydolabz.com/index.php?main_page=index&cPath=165"):
+		for u in ("all pages to be crawled"):
 			page=urllib2.urlopen(u)
 			soup = BeautifulSoup(page)
 					
-			productName = soup.findAll(attrs = 'itemTitle')
-			for p in productName:
-				productnames.append(p.contents[0].next)
-			productPrice = soup.findAll('td',attrs = {'class':"productListing-data", 'align':"right"})		
-			for p in productPrice:
+			getInfo = soup.findAll(attrs = 'itemTitle')
+			for p in getInfo:
+				infoList2.append(p.contents[0].next)
+			getInfo1 = soup.findAll('td',attrs = {'class':"class", 'align':"allignment"})		
+			for p in getInfo1:
 				try:
-					productprices.append(p.find(attrs = 'productSpecialPrice').string)	# price of product on sale					
+					infoList1.append(p.find(attrs = 'attribute').string)						
 				except:
-					productprices.append(p.next.next) # price of product without discount
+					infoList1.append(p.next.next) 
 			page.close()						
 		
-		return productnames,productprices
+		return infoList2,infoList1
 
 
-class catSystems:
+class site6:
 	
 	def get_data(self):
-		productprices = []
-		productnames = []
+		infoList1 = []
+		infoList2 = []
 		
-		for u in ("http://www.catsystems.in/products.aspx?qscatid=103","http://www.catsystems.in/products.aspx?qscatid=163","http://www.catsystems.in/products.aspx?qscatid=157","http://www.catsystems.in/products.aspx?qscatid=82","http://www.catsystems.in/products.aspx?qscatid=102","http://www.catsystems.in/products.aspx?qscatid=139","http://www.catsystems.in/products.aspx?qscatid=143","http://www.catsystems.in/products.aspx?qscatid=146","http://www.catsystems.in/products.aspx?qscatid=152","http://www.catsystems.in/products.aspx?qscatid=160","http://www.catsystems.in/products.aspx?qscatid=186","http://www.catsystems.in/products.aspx?qscatid=202","http://www.catsystems.in/products.aspx?qscatid=28","http://www.catsystems.in/products.aspx?qscatid=71","http://www.catsystems.in/products.aspx?qscatid=140","http://www.catsystems.in/products.aspx?qscatid=147","http://www.catsystems.in/products.aspx?qscatid=148","http://www.catsystems.in/products.aspx?qscatid=159"):
+		for u in ("relelvant pages"):
 			page=urllib2.urlopen(u)
 			soup = BeautifulSoup(page)
 					
-			productName = soup.findAll(attrs = {"style":"margin-bottom:5px"})
-			pr = iter(productName)
+			getInfo = soup.findAll(attrs = {"style":"margin-bottom:5px"})
+			pr = iter(getInfo)
 			for p in pr:
 				if p.contents[1].string != None:
-					productnames.append(p.contents[1].string)
+					infoList2.append(p.contents[1].string)
 					pr.next() # skip product id
 			
-			productPrice = soup.findAll(attrs = {'alt':'Rs.'})		
-			for p in productPrice:
-				productprices.append(p.next.next.string)						
+			getInfo1 = soup.findAll(attrs = {'alt':'Rs.'})		
+			for p in getInfo1:
+				infoList1.append(p.next.next.string)						
 
 					
 		page.close()
 		
-		return productnames,productprices	
+		return infoList2,infoList1	
 
-class tenet:
+class site7:
 	
 	def get_data(self):
-		productprices = []
-		productnames = []
+		infoList1 = []
+		infoList2 = []
 		
-		for u in ("http://tenettech.com/category.php?n=50&id_category=28","http://tenettech.com/category.php?id_category=68","http://tenettech.com/category.php?id_category=87","http://tenettech.com/category.php?id_category=75","http://tenettech.com/category.php?id_category=78","http://tenettech.com/category.php?id_category=72","http://tenettech.com/category.php?id_category=82","http://tenettech.com/category.php?id_category=83","http://tenettech.com/category.php?id_category=85","http://tenettech.com/category.php?id_category=88","http://tenettech.com/category.php?id_category=91"):
+		for u in ("pages to be crawled"):
 			page=urllib2.urlopen(u)
 			soup = BeautifulSoup(page)
 					
-			productName = soup.findAll('h3')
-			for p in productName:
+			getInfo = soup.findAll('h3')
+			for p in getInfo:
 				if p.next.next!='New' and p.next!= 'Subcategories':
-					productnames.append(p.next.next)
-			productPrice = soup.findAll(attrs = {'class':'price'})		
-			for p in productPrice:
-				productprices.append(p.next)						
+					infoList2.append(p.next.next)
+			getInfo1 = soup.findAll(attrs = {'class':'the class needed'})		
+			for p in getInfo1:
+				infoList1.append(p.next)						
 		page.close()
 		
-		return productnames,productprices	
+		return infoList2,infoList1	
 
